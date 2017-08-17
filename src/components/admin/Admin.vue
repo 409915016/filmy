c9fc8a69c6e8de29b33d4e05e12051f4ae6925f0
Mather <409915016@qq.com>
2017-08-18 00:43:25 +0800

    * Fix admin page router.

---
+++ src/components/admin/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <sidebar ref="sidebar"></sidebar>
+      </div>
+      <div class="col-md-9 pull-right">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Sidebar from './Sidebar.vue'
+  export default {
+    components: {
+      Sidebar
+    }
+  }
+</script>
