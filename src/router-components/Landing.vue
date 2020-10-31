c7888437346ff095c39bb3556cb5892364c619f8
mather <misaka@misaka.im>
2020-10-31 18:38:24 +0800

    格式化 Filmy 首页代码

--- src/router-components/Landing.vue
+++ src/router-components/Landing.vue
@@ -1,44 +1,42 @@
 <template>
-  <div id="landing">
-    <SearchBar :router="$route.router"></SearchBar>
-    <LandingView :config="config"></LandingView>
-    <Content :categories="categories"></Content>
-  </div>
+    <div id="landing">
+        <SearchBar :router="$route.router"></SearchBar>
+        <LandingView :config="config"></LandingView>
+        <Content :categories="categories"></Content>
+    </div>
 </template>
 
 <script>
-  import Config from '../models/Config'
-  import Category from '../models/Category'
-  import LandingView from '../components/LandingView.vue'
-  import Content from '../components/Content.vue'
-  import SearchBar from '../components/SearchBar.vue'
-  export default {
-    name: 'LandingPage',
-    data () {
-      return {
-        config: {},
-        categories: []
-      }
-    },
-    mounted () {
-      Promise.all([
-        Config.load(),
-        Category.load()
-      ])
-        .then(([config, categories]) => {
-          this.config = config
-          this.categories = categories
-//          this.$dispatch('update-blog-title', this.config.title)
-//          this.$dispatch('update-title', '')
-          // emit
-          this.$emit('update-blog-title', this.config.title)
-          this.$emit('update-title', '')
-        })
-    },
-    components: {
-      LandingView,
-      Content, // <Content :categories="categories"></Content>
-      SearchBar
+    import Config      from '@/models/Config'
+    import Category    from '@/models/Category'
+    import LandingView from '@/components/LandingView'
+    import Content     from '@/components/Content'
+    import SearchBar   from '@/components/SearchBar'
+
+    export default {
+        name      : 'LandingPage',
+        data() {
+            return {
+                config    : {},
+                categories: []
+            }
+        },
+        mounted() {
+            Promise.all([
+                Config.load(),
+                Category.load()
+            ])
+                .then(([config, categories]) => {
+                    this.config     = config
+                    this.categories = categories
+                    this.$emit('update-blog-title', this.config.title)
+                    this.$emit('update-title', '')
+                })
+        },
+        components: {
+            LandingView,
+            Content,
+            SearchBar
+        }
     }
-  }
 </script>
