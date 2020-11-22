bf70d345b8b6da8ce1bb54a3763334bff55a5d67
mather <misaka@misaka.im>
2020-11-22 12:03:06 +0800

    移除未使用的变量和过期的注释

--- src/models/Config.js
+++ src/models/Config.js
@@ -1,14 +1,7 @@
 import min         from 'min'
 import filmyBucket from './qiniu-bucket'
-//import qiniu from 'qiniu-js'
 import {isString}  from 'lodash'
-
-import { bucketName, qiniuBucketDomainName, qiniuAccessKey, qiniuSecretKey, qiniuUploadUrl as uploadUrl } from '@/configs'
-
-// 本地存在 核心配置数据 filmy:config 吗？
-// 存在 则获取本地数据
-// 不存在 则通过七牛云下载 config.json 然后保存在本地 filmy:config
-let _temp_config;
+import { bucketName } from '@/configs'
 
 const Config = {
     load (silent = false) {
@@ -38,20 +31,6 @@ const Config = {
           })
     },
 
-    // 通过七牛云密码来更新七牛云线上文件
-    // 核心配置数据:
-    // title
-    // description
-    // author
-    // avatar
-    // key 是 核心配置数据的键
-    // value 是核心配置数据的值
-    // ==============================================
-    // 先用密码获取 putToken
-    // 读取本地配置数据与 putToken 组成数组
-    // 根据传入要修改的 key 和 value 修改成新的 config
-    // 使用 Blob 构造类二进制文件对象
-    // 将文件对象使用 putFile 上传到七牛云
     update (password, update = {}, silent = false) {
         if (!isString(password)) {
             throw new TypeError('Password must be a string')
